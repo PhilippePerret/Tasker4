afa324e6d6baeae09bc5ccec828fe4ade79f568f
PhilippePerret <philippe.perret@yahoo.fr>
2025-02-13 10:04:52 +0100

    Juste avant la création de la table task_specs

--- priv/repo/migrations/20250212094854_create_tasks.exs
+++ priv/repo/migrations/20250212094854_create_tasks.exs
@@ -5,7 +5,7 @@ defmodule Tasker.Repo.Migrations.CreateTasks do
     create table(:tasks, primary_key: false) do
       add :id, :binary_id, primary_key: true
       add :title, :string
-      add :project_id, references(:projects, on_delete: :nothing, type: :binary_id)
+      add :project_id, references(:projects, on_delete: :delete_all, type: :binary_id)
 
       timestamps(type: :utc_datetime)
     end
