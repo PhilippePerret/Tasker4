69360777082257f8f8cb107debbee821d5501ac8
PhilippePerret <philippe.perret@yahoo.fr>
2025-02-14 09:41:35 +0100

    Traitement des messages flash

--- lib/tasker_web/components/core_components.ex
+++ lib/tasker_web/components/core_components.ex
@@ -101,7 +101,7 @@ defmodule TaskerWeb.CoreComponents do
   attr :id, :string, doc: "the optional id of flash container"
   attr :flash, :map, default: %{}, doc: "the map of flash messages to display"
   attr :title, :string, default: nil
-  attr :kind, :atom, values: [:info, :error], doc: "used for styling and flash lookup"
+  attr :kind, :atom, values: [:info, :error, :notice], doc: "used for styling and flash lookup"
   attr :rest, :global, doc: "the arbitrary HTML attributes to add to the flash container"
 
   slot :inner_block, doc: "the optional inner block that renders the flash message"
@@ -115,22 +115,16 @@ defmodule TaskerWeb.CoreComponents do
       id={@id}
       phx-click={JS.push("lv:clear-flash", value: %{key: @kind}) |> hide("##{@id}")}
       role="alert"
-      class={[
-        "fixed top-2 right-2 mr-2 w-80 sm:w-96 z-50 rounded-lg p-3 ring-1",
-        @kind == :info && "bg-emerald-50 text-emerald-800 ring-emerald-500 fill-cyan-900",
-        @kind == :error && "bg-rose-50 text-rose-900 shadow-md ring-rose-500 fill-rose-900"
-      ]}
+      class={@kind}
       {@rest}
     >
-      <p :if={@title} class="flex items-center gap-1.5 text-sm font-semibold leading-6">
-        <.icon :if={@kind == :info} name="hero-information-circle-mini" class="h-4 w-4" />
-        <.icon :if={@kind == :error} name="hero-exclamation-circle-mini" class="h-4 w-4" />
-        <%= @title %>
-      </p>
-      <p class="mt-2 text-sm leading-5"><%= msg %></p>
-      <button type="button" class="group absolute top-1 right-1 p-2" aria-label={gettext("close")}>
-        <.icon name="hero-x-mark-solid" class="h-5 w-5 opacity-40 group-hover:opacity-70" />
-      </button>
+      <p :if={@title} class="title"><%= @title %></p>
+      <p class="message"><%= msg %></p>
+      <button 
+        type="button"
+        onclick="this.parentElement.remove()"
+        class="close-btn" 
+        aria-label={gettext("close")}></button>
     </div>
     """
   end
@@ -148,8 +142,9 @@ defmodule TaskerWeb.CoreComponents do
   def flash_group(assigns) do
     ~H"""
     <div id={@id}>
-      <.flash kind={:info} title={gettext("Success!")} flash={@flash} />
-      <.flash kind={:error} title={gettext("Error!")} flash={@flash} />
+      <.flash kind={:info}} flash={@flash} />
+      <.flash kind={:error} flash={@flash} />
+      <.flash kind={:notice} flash={@flash} />
       <.flash
         id="client-error"
         kind={:error}
@@ -159,7 +154,6 @@ defmodule TaskerWeb.CoreComponents do
         hidden
       >
         <%= gettext("Attempting to reconnect") %>
-        <.icon name="hero-arrow-path" class="ml-1 h-3 w-3 animate-spin" />
       </.flash>
 
       <.flash
@@ -171,7 +165,6 @@ defmodule TaskerWeb.CoreComponents do
         hidden
       >
         <%= gettext("Hang in there while we get back on track") %>
-        <.icon name="hero-arrow-path" class="ml-1 h-3 w-3 animate-spin" />
       </.flash>
     </div>
     """
--- lib/tasker_web/controllers/Page/page_controller.ex
+++ lib/tasker_web/controllers/Page/page_controller.ex
@@ -1,9 +1,8 @@
 defmodule TaskerWeb.PageController do
   use TaskerWeb, :controller
-
   
   def home(conn, _params) do
-    # IO.inspect(Gettext.get_locale(TaskerWeb.Gettext), label: "LOCALE")
-    render(conn, :home)
+    conn
+    |> render(:home)
   end
 end
