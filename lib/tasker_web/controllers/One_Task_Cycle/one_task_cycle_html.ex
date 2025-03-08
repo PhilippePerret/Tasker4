a73cc40830db084f3124b82aca74f5baac1f6096
PhilippePerret <philippe.perret@yahoo.fr>
2025-03-08 02:10:56 +0100

    Pour éviter l'erreur de formatage VSCode

--- lib/tasker_web/controllers/One_Task_Cycle/one_task_cycle_html.ex
+++ lib/tasker_web/controllers/One_Task_Cycle/one_task_cycle_html.ex
@@ -5,4 +5,13 @@ defmodule TaskerWeb.OneTaskCycleHTML do
 
   embed_templates "one_task_cycle_html/*"
 
+  # Juste pour éviter l'erreur de formatage VSCode
+  slot :inner_block, required: true
+  def js_constants(assigns) do
+    ~H"""
+    <script type="text/javascript">
+      <%= render_slot(@inner_block) %>
+    </script>
+    """
+  end
 end
