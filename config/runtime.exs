9e4d4ad44a666e10df510d64442b5d30a3779799
PhilippePerret <philippe.perret@yahoo.fr>
2025-03-14 06:12:09 +0100

    Correction bug sur formulaire inscription (absence pseudo)

--- config/runtime.exs
+++ config/runtime.exs
@@ -21,40 +21,41 @@ if System.get_env("PHX_SERVER") do
 end
 
 if config_env() == :prod do
-  database_url =
-    System.get_env("DATABASE_URL") ||
-      raise """
-      environment variable DATABASE_URL is missing.
-      For example: ecto://USER:PASS@HOST/DATABASE
-      """
 
-  maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
+  # maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
   config :tasker, Tasker.Repo,
     # ssl: true,
-    url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    socket_options: maybe_ipv6
+    # url: database_url,
+    # socket_options: maybe_ipv6
+    username: "postgres",
+    password: "postgres",
+    hostname: "localhost",
+    database: "tasker",
+    pool_size: 10
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
   # want to use a different value for prod and you most likely don't want
   # to check this value into version control, so we use an environment
   # variable instead.
-  secret_key_base =
-    System.get_env("SECRET_KEY_BASE") ||
-      raise """
-      environment variable SECRET_KEY_BASE is missing.
-      You can generate one by calling: mix phx.gen.secret
-      """
+  # secret_key_base =
+  #   System.get_env("SECRET_KEY_BASE") ||
+  #     raise """
+  #     environment variable SECRET_KEY_BASE is missing.
+  #     You can generate one by calling: mix phx.gen.secret
+  #     """
+  secret_key_base = "7EijiZg9cgAXNMTEjPR7Nj39xiilpw+RdFn3SX25xJnpj+sYm6w6rv2l8wYz+aLb"
 
-  host = System.get_env("PHX_HOST") || "example.com"
-  port = String.to_integer(System.get_env("PORT") || "4000")
+  # host = System.get_env("PHX_HOST") || "localhost.com"
+  host = "localhost"
+  # port = String.to_integer(System.get_env("PORT") || "4000")
+  port = "4001"
 
   config :tasker, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :tasker, TaskerWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    # url: [host: host, port: 443, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
@@ -63,6 +64,9 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
+    code_reloader: false,
+    check_origin: false,
+    debug_errors: false,
     secret_key_base: secret_key_base
 
   # ## SSL Support
