b36383215512943f7bcf2efe0c4d622e156920d0
PhilippePerret <philippe.perret@yahoo.fr>
2025-03-15 15:25:57 +0100

    Fixed #55 (noms dans les projets)

--- lib/tasker_web/controllers/Projet/project_html.ex
+++ lib/tasker_web/controllers/Projet/project_html.ex
@@ -10,4 +10,11 @@ defmodule TaskerWeb.ProjectHTML do
   attr :action, :string, required: true
 
   def project_form(assigns)
+
+  attr :project, :string, required: true
+  def project_title(assigns) do
+    ~H"""
+    <%= gettext("OPEN_GUIL") %>{@project.title}<%= gettext("CLOSE_GUIL") %>
+    """
+  end
 end
